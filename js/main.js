684fa9704c82fbb02a6586a59ed1b47b3e2ed756
Baldemar Sepulveda <balde.sepu@pm.me>
2019-09-02 16:56:52 -0700

    New UI Complete along with refactoring done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,112 @@
+const MAX_ROUNDS = 5;
+let currentRound = 1;
+
+let playerScore = 0;
+let computerScore = 0;
+
+const roundCountElement = document.getElementById("round-count");
+const playerScoreElement = document.getElementById("player-score");
+const computerScoreElement = document.getElementById("computer-score");
+
+roundCountElement.innerText = currentRound;
+playerScoreElement.innerText = playerScore;
+computerScoreElement.innerText = computerScore;
+
+const roundOutputElement = document.getElementById("round-output");
+const roundDrawElement = document.getElementById("draw");
+const gameResultElement = document.getElementById("game-results");
+
+document.querySelectorAll(".player-selection").forEach(selection => {
+  selection.addEventListener("click", game);
+});
+
+function game(e) {
+  if (currentRound > MAX_ROUNDS) {
+    return;
+  }
+  const playerSelection = this.dataset.play;
+  const computerSelection = computerPlay();
+  const roundResult = playRound(playerSelection, computerSelection);
+
+  if (roundResult.winLoseState === "Win") {
+    updatePlayerScore();
+  } else if (roundResult.winLoseState === "Lose") {
+    updateComputerScore();
+  }
+
+  updateRound();
+
+  if (currentRound > MAX_ROUNDS) {
+    roundCountElement.innerText = "Game Over";
+    if (playerScore > computerScore) {
+      document.getElementById("game-win-lose").innerText = "Win";
+      gameResultElement.children[0].classList.add("success");
+      gameResultElement.style.display = gameResultElement.dataset.display;
+    } else if (playerScore < computerScore) {
+      document.getElementById("game-win-lose").innerText = "Lose";
+      gameResultElement.children[0].classList.add("failure");
+      gameResultElement.style.display = gameResultElement.dataset.display;
+    } else {
+      document.getElementById("game-win-lose").innerText = "Got A Draw";
+      gameResultElement.children[0].classList.add("success");
+      gameResultElement.style.backgroundColor = "gold";
+      gameResultElement.style.display = gameResultElement.dataset.display;
+    }
+    return;
+  }
+
+  displayRoundResult(roundResult);
+}
+
+function updatePlayerScore(increment = 1) {
+  playerScore += increment;
+  playerScoreElement.innerText = playerScore;
+}
+
+function updateComputerScore(increment = 1) {
+  computerScore += increment;
+  computerScoreElement.innerText = computerScore;
+}
+
+function updateRound(increment = 1) {
+  currentRound += increment;
+  roundCountElement.innerText = currentRound;
+}
+
+function displayRoundResult(result) {
+  const winLoseState = result.winLoseState;
+  let winningPlay;
+  let losingPlay;
+  if (winLoseState === "Win") {
+    winningPlay = result.roundWinningPlay;
+    losingPlay = result.roundLosingPlay;
+    roundOutputElement.children[0].classList.add("success");
+    document.getElementById("round-win-lose").innerText = winLoseState;
+    document.getElementById("round-winner").innerText = winningPlay;
+    document.getElementById("round-loser").innerText = losingPlay;
+    roundOutputElement.style.display = roundOutputElement.dataset.display;
+    setTimeout(() => {
+      roundOutputElement.style.display = "none";
+    }, 1000);
+  } else if (winLoseState === "Lose") {
+    winningPlay = result.roundWinningPlay;
+    losingPlay = result.roundLosingPlay;
+    roundOutputElement.children[0].classList.add("failure");
+    document.getElementById("round-win-lose").innerText = winLoseState;
+    document.getElementById("round-winner").innerText = winningPlay;
+    document.getElementById("round-loser").innerText = losingPlay;
+    roundOutputElement.style.display = roundOutputElement.dataset.display;
+    setTimeout(() => {
+      roundOutputElement.style.display = "none";
+    }, 1000);
+  } else if (winLoseState === "Draw") {
+    roundDrawElement.style.display = roundDrawElement.dataset.display;
+    setTimeout(() => {
+      roundDrawElement.style.display = "none";
+    }, 1000);
+  }
+}
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -69,76 +178,36 @@ function versusScissors(play) {
   }
 }
 
-function game(maxRounds = 5) {
-  let round = 1;
-
-  let playerSelection;
-  let computerSelection;
-
-  let playerWins = 0;
-  let computerWins = 0;
-
-  for (let i = round; i <= maxRounds; i++) {
-    playerSelection = prompt(`Round: ${i} || Enter Rock, Paper, or Scissors`);
-
-    while (
-      !playerSelection ||
-      (!isPaper(playerSelection) &&
-        !isRock(playerSelection) &&
-        !isScissors(playerSelection))
-    ) {
-      playerSelection = prompt(
-        "Enter valid play!!! Enter Rock, Paper, or Scissors"
-      );
-    }
-    computerSelection = computerPlay();
-
-    roundResult = playRound(playerSelection, computerSelection);
-    alert(roundResult.text);
-    if (roundResult.result < 0) {
-      computerWins++; // player looses
-    } else if (roundResult.result > 0) {
-      playerWins++; // computer looses
-    }
-  }
-
-  if (playerWins > computerWins) {
-    alert(`You Win!!! Player: ${playerWins} Computer: ${computerWins}`);
-  } else if (playerWins < computerWins) {
-    alert(`You Loose!!! Player: ${playerWins} Computer: ${computerWins}`);
+function playRound(playerSelection, computerSelection) {
+  let result;
+  if (isRock(playerSelection)) {
+    result = versusRock(computerSelection);
+  } else if (isPaper(playerSelection)) {
+    result = versusPaper(computerSelection);
+  } else if (isScissors(playerSelection)) {
+    result = versusScissors(computerSelection);
   } else {
-    alert(`Draw!!! Player: ${playerWins} Computer: ${computerWins}`);
+    return "invalid input";
   }
+  return getRoundResult(result, playerSelection, computerSelection);
+}
 
-  function playRound(playerSelection, computerSelection) {
-    let result;
-    if (isRock(playerSelection)) {
-      result = versusRock(computerSelection);
-    } else if (isPaper(playerSelection)) {
-      result = versusPaper(computerSelection);
-    } else if (isScissors(playerSelection)) {
-      result = versusScissors(computerSelection);
-    } else {
-      return "invalid input";
-    }
-    return getRoundResult(result, playerSelection, computerSelection);
-  }
-
-  function getRoundResult(result, playerSelection, computerSelection) {
-    playerSelection = capitalize(playerSelection);
-    if (result < 0) {
-      return {
-        text: `You Lose! ${computerSelection} beats ${playerSelection} `,
-        result
-      };
-    } else if (result === 0) {
-      return { text: `No Winner! Draw`, result };
-    } else {
-      return {
-        text: `You Win! ${playerSelection} beats ${computerSelection}`,
-        result
-      };
-    }
+function getRoundResult(result, playerSelection, computerSelection) {
+  playerSelection = capitalize(playerSelection);
+  if (result < 0) {
+    return {
+      roundWinningPlay: computerSelection,
+      roundLosingPlay: playerSelection,
+      winLoseState: "Lose"
+    };
+  } else if (result === 0) {
+    return { winLoseState: "Draw" };
+  } else {
+    return {
+      roundWinningPlay: playerSelection,
+      roundLosingPlay: computerSelection,
+      winLoseState: "Win"
+    };
   }
 }
 
